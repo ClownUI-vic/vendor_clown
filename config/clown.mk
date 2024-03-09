8655361b8a0f75cb9bb62f4c43be2d522192a5b1
Pranav Vashi <neobuddy89@gmail.com>
2024-03-09 14:20:19 +0530

    config: Build OmniStyle
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>

--- config/clown.mk
+++ config/clown.mk
@@ -1,11 +1,12 @@
 # ClownUI packages
 PRODUCT_PACKAGES += \
-    GameSpace
+    GameSpace \
+    OmniStyle
 
 # BtHelper
 PRODUCT_PACKAGES += \
     BtHelper
-    
+
 # Cloned app exemption
 PRODUCT_COPY_FILES += \
     vendor/lineage/prebuilt/common/etc/sysconfig/preinstalled-packages-platform-clown-product.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/preinstalled-packages-platform-clown-product.xml
