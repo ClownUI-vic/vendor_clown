7a13c10aacaab1acb59d07efe70319599f4581f8
someone5678 <someone5678@users.noreply.github.com>
2024-05-22 04:38:52 +0900

    config: Build BtHelper
    Signed-off-by: Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>

--- config/clown.mk
+++ config/clown.mk
@@ -2,6 +2,10 @@
 PRODUCT_PACKAGES += \
     GameSpace
 
+# BtHelper
+PRODUCT_PACKAGES += \
+    BtHelper
+    
 # Cloned app exemption
 PRODUCT_COPY_FILES += \
     vendor/lineage/prebuilt/common/etc/sysconfig/preinstalled-packages-platform-clown-product.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/preinstalled-packages-platform-clown-product.xml
